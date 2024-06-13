186e525d7647f29fec4cc744d0d1185ccc05c522
AHenry79 <austinhenry7799@gmail.com>
2024-06-13 15:15:45 -0400

    Create seed

---
+++ db/index.js
@@ -0,0 +1,51 @@
+const pg = require("pg");
+
+const client = new pg.Client("postgres://localhost/ecommerce");
+
+const getAllUsers = async () => {
+  const response = await client.query(`SELECT * FROM users ORDER BY id ASC`);
+  return response.rows;
+};
+const getAllProducts = async () => {
+  const response = await client.query(`SELECT * FROM product ORDER BY id ASC`);
+  return response.rows;
+};
+const getCartItemsByUserId = async (params_id) => {
+  const response = await client.query(`SELECT * FROM cart WHERE user_id=$1`, [
+    params_id,
+  ]);
+  return {
+    cart: response.rows,
+  };
+};
+const getSingleUserById = async (id) => {
+  const response = await client.query(`SELECT * FROM users WHERE id = $1`, [
+    id,
+  ]);
+  return response.rows[0];
+};
+const addToCartByUserId = async (body) => {
+  await client.query(`INSERT INTO cart(product_id, user_id) VALUES($1, $2)`, [
+    body.product_id,
+    body.user_id,
+  ]);
+  return {
+    product_id: body.product_id,
+    user_id: body.user_id,
+  };
+};
+const deleteCartItemByUserId = async (id) => {
+  await client.query(`DELETE FROM cart WHERE id = $1`, [Number(id)]);
+  return {
+    id: id,
+  };
+};
+module.exports = {
+  getAllUsers,
+  getAllProducts,
+  getSingleUserById,
+  getCartItemsByUserId,
+  addToCartByUserId,
+  deleteCartItemByUserId,
+  client,
+};
---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const { client } = require("./db/index");
+
+app.use(express.json());
+client.connect();
+
+// app.use("/api/users", require("./users"));
+// app.use("/api/products", require("./products"));
+// app.use("/api/favorites", require("./favorites"));
+
+app.listen(8080, () => {
+  console.log("App is running at port 8080");
+});
