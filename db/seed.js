73558baa345d1925db9afde98ea974e0f4fb554d
AHenry79 <austinhenry7799@gmail.com>
2024-06-14 11:33:46 -0400

    Add orders table

--- db/seed.js
+++ db/seed.js
@@ -9,6 +9,7 @@ async function dropTables() {
         DROP TABLE IF EXISTS cart;
         DROP TABLE IF EXISTS products;
         DROP TABLE IF EXISTS users;
+        DROP TABLE IF EXISTS orders;
       `);
 
     console.log("Finished dropping tables!");
@@ -51,6 +52,14 @@ async function createTables() {
         "customer_id" int4 NOT NULL,
         PRIMARY KEY ("id")
         );
+
+         CREATE TABLE "orders" (
+        "id" SERIAL NOT NULL,
+        "product_id" int4 NOT NULL,
+        "customer_id" int4 NOT NULL,
+        "ordered_at" timestamptz NOT NULL DEFAULT now(),
+        PRIMARY KEY ("id")
+        );
       `);
 
     console.log("Finished building tables!");
@@ -67,7 +76,7 @@ async function createInitialUsers() {
     await client.query(`
         CREATE UNIQUE INDEX users_username_idx ON users USING btree (username);
 
-        INSERT INTO "public"."users" ("id", "username", "password", "email", "phoneNumber", "created_at", "updated_at") VALUES
+        INSERT INTO "users" ("id", "username", "password", "email", "phoneNumber", "created_at", "updated_at") VALUES
         (5, 'sam23', 'pass123', 'sam233@gmail.com', '718-928-2383', '2024-06-13 14:28:44.156062-04', '2024-06-13 14:28:44.156062-04'),
         (6, 'peter2434', 'pass333', 'pete5@gmail.com', '888-923-3283', '2024-06-13 14:28:44.156062-04', '2024-06-13 14:28:44.156062-04'),
         (7, 'matt343', 'mattp09@gmail.com', 'mparker56@gmail.com', '029-238-2382', '2024-06-13 14:28:44.156062-04', '2024-06-13 14:28:44.156062-04');
@@ -82,7 +91,7 @@ async function createInitialProducts() {
   await client.query(`
            CREATE UNIQUE INDEX product_pkey ON products USING btree (id);
 
-            INSERT INTO "public"."products" ("id", "price", "desciption", "name", "categories", "image_url", "availability") VALUES
+            INSERT INTO "products" ("id", "price", "desciption", "name", "categories", "image_url", "availability") VALUES
             (4, 5.99, 'fruit', 'blueberries', 'produce', 'https://i5.walmartimages.com/asr/206db476-ca7d-4919-a4a5-c25dd2ae5feb_1.f7927999cdacc320a6c5a83462cadbd5.jpeg?odnHeight=768&odnWidth=768&odnBg=FFFFFF', 't'),
             (14, 1.99, 'fruit', 'bannas', 'produce', 'https://www.walmart.com/ip/Marketside-Fresh-Organic-Bananas-Bunch/51259338?athbdg=L1200', 't'),
             (16, 5.44, 'fruit', 'peaches', 'produce', 'https://www.walmart.com/ip/Fresh-Organic-Peaches-2-lb-Bag/157005099', 't'),
@@ -104,16 +113,29 @@ async function createInitialProducts() {
 }
 async function createInitialCartItems() {
   try {
-    console.log("Starting to create posts...");
+    console.log("Starting to create cart items...");
     await client.query(`
         INSERT INTO "cart" ("id", "product_id", "customer_id") VALUES
         (2, 27, 7),
         (3, 28, 5),
-        (8, 14, 7);
+        (8, 14, 7)
         `);
     console.log("Finished creating cart items!");
   } catch (error) {
     console.log("Error creating cart items!");
+    throw error;
+  }
+}
+async function createInitialOrderedItems() {
+  try {
+    console.log("Starting to create ordered items...");
+    await client.query(`
+        INSERT INTO "orders" ("product_id", "customer_id") VALUES
+        (14, 7)
+        `);
+    console.log("Finished creating ordered items!");
+  } catch (error) {
+    console.log("Error creating ordered items!");
     throw error;
   }
 }
@@ -127,6 +149,7 @@ async function rebuildDB() {
     await createInitialUsers();
     await createInitialCartItems();
     await createInitialProducts();
+    await createInitialOrderedItems();
   } catch (error) {
     console.log("Error during rebuildDB");
     throw error;
