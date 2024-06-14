d19126e08955aab6d388829cf55be77d6cf38790
jrosan9 <jbrosan4@gmail.com>
2024-06-14 11:41:19 -0400

    changes

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ app.use(express.json());
 client.connect();
 
 app.use("/api/users", require("./api/users"));
-// app.use("/api/products", require("./products"));
+app.use("/api/products", require("./products"));
 app.use("/api/", require("./api/cart.js"));
 
-app.listen(8080, () => {
-  console.log("App is running at port 8080");
+app.listen(4000, () => {
+  console.log("App is running at port 4000");
 });
